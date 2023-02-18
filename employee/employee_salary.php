3016fa0eda069a4027e711e8e449acadd505149f
Ayushi Mehta <109412787+ayushisanjaymehta@users.noreply.github.com>
2023-02-18 13:03:13 +0530

    Add files via upload

---
+++ employee/employee_salary.php
@@ -0,0 +1,38 @@
+<?php
+	include "index.php";
+	include "config.php";
+	$sql = "select * from `employee` where `salary`<2000";
+	$res = mysqli_query($con,$sql);
+	?>	
+		<div class="container mt-2">
+			<table class="table table-bordered text-center">
+				<tr>
+					<th>#</th>
+					<th>Name</th>
+					<th>Department</th>
+					<th>Salary</th>
+					<th>Leave</th>
+					<th>Joining Date</th>
+					<th>Resign Date</th>
+				</tr>
+				<?php
+					$i = 0;
+					while($row = mysqli_fetch_assoc($res))
+					{
+						$i++;
+						?>
+							<tr>
+								<td><?php echo $i; ?></td>
+								<td><?php echo $row['employee_name']; ?></td>
+								<td><?php echo $row['department']; ?></td>
+								<td><?php echo $row['salary']; ?></td>
+								<td><?php echo $row['emp_leave']; ?></td>
+								<td><?php echo $row['joining_date']; ?></td>
+								<td><?php echo $row['resign_data']; ?></td>
+							</tr>
+						<?php
+					}
+					mysqli_close($con);
+	?>			
+			</table>
+		</div>
