8c767446eaaf373cc2195ed81adb6c4de3f65391
Ayushi Mehta <109412787+ayushimehta020@users.noreply.github.com>
2023-10-12 21:07:40 +0530

    Add files via upload

---
+++ htdocs/index.php
@@ -0,0 +1,12 @@
+<?php
+    include "config.php";
+?>
+
+<div class="container mt-2 rounded-2 border border-dark bg-dark">
+    <form action="index.php" method="POST" class="mt-2">
+        <div class="input-group mb-3 mt-3">
+            <span class="input-group-text" id="basic-addon1">Enter your name</span>
+            <input type="text" class="form-control" name="txtnm" placeholder="Name" aria-label="Name" aria-describedby="Enter your name" required>
+        </div>
+    </form>
+</div>
